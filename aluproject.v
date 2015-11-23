2f52a78e9066a59b3fb775f84ad30794c8a876e2
mertesackerEmptiness <f2013044@goa.bits-pilani.ac.in>
2015-11-24 03:24:22 +0530

    simple_adder

--- aluproject.v
+++ aluproject.v
@@ -54,3 +54,10 @@ module adder(input [31:0] in1, input [31:0] in2, output reg [31:0] adder_out, ou
 			overflow=1'b1;
 		end
 endmodule
+
+module simple_adder(input [31:0] in1, input [31:0] in2, output reg [31:0] adder_out);
+	always@(in1,in2)
+	begin
+		adder_out = in1 + in2;
+	end
+endmodule
