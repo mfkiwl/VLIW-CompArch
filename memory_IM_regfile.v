7368c3d806d679c4dc169aa1d130f36a5bd977ee
mertesackerEmptiness <f2013044@goa.bits-pilani.ac.in>
2015-11-24 03:20:37 +0530

    mux16to1

--- memory_IM_regfile.v
+++ memory_IM_regfile.v
@@ -30,6 +30,30 @@ module register_Mem(input clk,input reset,input regWrite,input decOut1b,input [7
 
 
 endmodule
+
+module mux16to1( input [15:0] outR0,outR1,outR2,outR3,outR4,outR5,outR6,outR7,outR8,outR9,outR10,outR11,outR12,outR13,outR14,outR15, input [3:0] Sel, output reg [15:0] outBus );
+	always@(outR0 or outR1 or outR2 or outR3 or outR4 or outR5 or outR6 or outR7 or outR8 or outR9 or outR10 or outR11 or outR12 or outR13 or outR14 or outR15 or Sel)
+	case (Sel)
+				4'b0000: outBus=outR0;
+				4'b0001: outBus=outR1;
+				4'b0010: outBus=outR2;
+				4'b0011: outBus=outR3;
+				4'b0100: outBus=outR4;
+				4'b0101: outBus=outR5;
+				4'b0110: outBus=outR6;
+				4'b0111: outBus=outR7;
+				4'b1000: outBus=outR8;
+				4'b1001: outBus=outR9;
+				4'b1010: outBus=outR10;
+				4'b1011: outBus=outR11;
+				4'b1100: outBus=outR12;
+				4'b1101: outBus=outR13;
+				4'b1110: outBus=outR14;
+				4'b1111: outBus=outR15;
+	endcase
+endmodule
+
+
 module Instr_Mem(input clk, input reset,input memWrite,input memRead, input [31:0] pc, input [31:0] dataIn,output [31:0] IR );
 	wire [15:0] Qout0, Qout1, Qout2, Qout3, Qout4, Qout5, Qout6, Qout7,
 					Qout8, Qout9, Qout10, Qout11, Qout12, Qout13, Qout14, Qout15,decOut;
